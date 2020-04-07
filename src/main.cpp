b10d596f6ec63ae7d6917485bcb207ddad20848d
Andreas Horvath <mail@andreas-horvath.at>
2020-04-07 20:03:16 +0200

    Fix compile problems with nana

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+
 #include "config.h"
 #include "job.h"
 #include "receipent.h"
 
 using namespace std;
+using namespace nana;
 
 int main()
 {
     Config cfg{};
     Job job{};
     Receipent r{};
-    cout << "hello world" << endl;
+
+    form fm;
+
+    label lb{ fm, rectangle{ 10, 10, 100, 100 } };
+    lb.caption("Hello, world!");
+    
+    fm.show();
+    
+    exec();
 }
