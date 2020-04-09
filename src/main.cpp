f814d6cc83accd82d14ddccfea822a7bf31f5c7b
Andreas Horvath <mail@andreas-horvath.at>
2020-04-09 23:10:56 +0200

    Add sqlitecpp dependency

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/label.hpp>
 
+#include <SQLiteCpp/SQLiteCpp.h>
+
 #include "config.h"
 #include "job.h"
 #include "receipent.h"
@@ -16,12 +18,14 @@ int main()
     Job job{};
     Receipent r{};
 
+    SQLite::Database db("test.db3", SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+
     form fm;
 
-    label lb{ fm, rectangle{ 10, 10, 100, 100 } };
+    label lb{fm, rectangle{10, 10, 100, 100}};
     lb.caption("Hello, world!");
-    
+
     fm.show();
-    
+
     exec();
 }
