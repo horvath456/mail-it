3f6cdc5f880faccf4eabb465fd504e10f7ec067b
Andreas Horvath <mail@andreas-horvath.at>
2020-04-23 17:42:51 +0200

    Add default host and port

--- include/config.h
+++ include/config.h
@@ -2,7 +2,12 @@
 
 #include <string>
 
-struct Config {
+struct Config
+{
+    Config() : host{"smtp.office365.com"}, port{587}, username{}, passwd{}, tmplate{}
+    {
+    }
+
     std::string host;
     int port;
     std::string username;
